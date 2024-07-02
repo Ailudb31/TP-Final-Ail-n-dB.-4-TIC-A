997e21233f9a6fddf406f71ca86b669a3f85d81d
Ailu <147621906+Ailudb31@users.noreply.github.com>
2024-07-02 08:56:55 -0300

    Assets y Código

---
+++ Assets/ArrayObjetos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayObjetos : MonoBehaviour
+{
+    public GameObject[] objetos;
+    int orden;
+
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[orden].SetActive(false);
+            orden--;
+            if (orden == -1)
+            {
+                orden = objetos.Length - 1;
+            }
+            objetos[orden].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            objetos[orden].SetActive(false);
+            orden++;
+            if (orden == objetos.Length)
+            {
+                orden = 0;
+            }
+            objetos[orden].SetActive(true);
+        }
+    }
+}
